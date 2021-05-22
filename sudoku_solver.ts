30163e57d3c0bf765128ead23a18489e6b8abf77
WebDev402 <yawebdev402@gmail.com>
2021-05-22 14:22:03 +0300

    change Sudoku solver initialization
    * creating class with init values as an array of number arrays
    * validating init values

--- sudoku_solver.ts
+++ sudoku_solver.ts
@@ -1,21 +1,56 @@
-class Sudoku {
-  grid: Cell[][];
-  m: number;
-  n: number;
+const FIELD_SIZE = 9;
 
-  constructor(n: number, m: number) {
-    this.n = n;
-    this.m = m;
-    this.grid = new Array(n).fill(0).map((_, x) => {
-      return new Array(m).fill(0).map((_, y) => new Cell(x, y));
+class SudokuSolver {
+  field: Cell[][];
+
+  constructor(initValues: number[][]) {
+    const [isValid, message] = this.isValidInitValues(initValues);
+    if (!isValid) {
+      throw new Error(message);
+    }
+
+    this.field = new Array(FIELD_SIZE).fill(0).map((_, x) => {
+      return new Array(FIELD_SIZE)
+        .fill(0)
+        .map((_, y) => new Cell(x, y, initValues[x][y]));
     });
+  }
+
+  private isValidInitValues(initValues: number[][]): [boolean, string] {
+    if (!Array.isArray(initValues)) {
+      return [false, 'Initialize value is not an array'];
+    }
+
+    if (initValues.length !== FIELD_SIZE) {
+      return [false, 'Initialize value length is not correct'];
+    }
+
+    if (!initValues.every((e) => Array.isArray(e))) {
+      return [false, 'Initialize value contain incorrect type item'];
+    }
+
+    if (!initValues.every((e) => e.length === FIELD_SIZE)) {
+      return [false, 'Initialize value item has incorrect length'];
+    }
+
+    if (
+      !initValues.every((e) =>
+        e.every((e) => {
+          return Number.isSafeInteger(e) && e >= 0 && e < 10;
+        })
+      )
+    ) {
+      return [false, 'Initialize value item contain incorrect value'];
+    }
+
+    return [true, ''];
   }
 
   public toString(): string {
     const field: string[] = [];
-    const separator = new Array(this.m + 1).fill('+').join('-');
+    const separator = new Array(FIELD_SIZE + 1).fill('+').join('-');
     field.push(separator);
-    this.grid.forEach((e) => {
+    this.field.forEach((e) => {
       field.push('|' + e.join('|') + '|');
       field.push(separator);
     });
@@ -26,20 +61,29 @@ class Sudoku {
 class Cell {
   possibleValues: number[];
   isFound: boolean = false;
-  x: number;
-  y: number;
-  value: number;
 
-  constructor(x: number, y: number, value: number = 0) {
-    this.x = x;
-    this.y = y;
-    this.value = value || (Math.random() * 10) | 0;
-  }
+  constructor(
+    private readonly x: number,
+    private readonly y: number,
+    private value: number = 0
+  ) {}
 
   public toString(): string {
     return this.value ? '' + this.value : ' ';
   }
 }
 
-const sudoku = new Sudoku(5, 8);
+var puzzle = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+];
+
+const sudoku = new SudokuSolver(puzzle);
 console.log(`${sudoku}`);

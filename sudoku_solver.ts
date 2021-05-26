1bd72e2e36a9fa99b338353170129c4185c70a28
WebDev402 <yawebdev402@gmail.com>
2021-05-26 20:15:57 +0300

    change isFound from property to method

--- sudoku_solver.ts
+++ sudoku_solver.ts
@@ -27,13 +27,16 @@ class SudokuSolver {
 
 class Cell {
   possibleValues: number[];
-  isFound: boolean = false;
 
   constructor(
     private readonly x: number,
     private readonly y: number,
     private value: number = 0
   ) {}
+
+  public isFound(): boolean {
+    return this.value > 0;
+  }
 
   public toString(): string {
     return this.value ? '' + this.value : ' ';
